70e37ed8791012998191fb173253e85a2ab4ba2d
Khadaassi <aassikhadija@yahoo.fr>
2025-07-07 10:01:35 +0200

    Refactor News model for improved readability and formatting

--- Bamk/news/models.py
+++ Bamk/news/models.py
@@ -2,17 +2,22 @@ from django.db import models
 from user.models import User
 from django.core.exceptions import ValidationError
 
+
 class News(models.Model):
     title = models.CharField(max_length=255)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
-    description = models.TextField( blank=True, null=True)
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True
+    )
+    description = models.TextField(blank=True, null=True)
     content = models.TextField()
-    image = models.ImageField(upload_to='news_images/', blank=True, null=True)
+    image = models.ImageField(upload_to="news_images/", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
     def save(self, *args, **kwargs):
         if not self.created_by.is_staff:
-            raise ValidationError("Seuls les utilisateurs staff peuvent créer des articles.")
+            raise ValidationError(
+                "Seuls les utilisateurs staff peuvent créer des articles."
+            )
         super().save(*args, **kwargs)
 
     def __str__(self):
